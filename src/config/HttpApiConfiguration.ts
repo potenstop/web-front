26a04228e94628a43095ffa0a544894e67bd04fe
yanshaowen <yanshaowen@zhiketong.cn>
2019-07-03 13:12:49 +0800

    修改api地址

--- src/config/HttpApiConfiguration.ts
+++ src/config/HttpApiConfiguration.ts
@@ -17,7 +17,7 @@ export class HttpApiConfiguration {
     const httpDataSource = new AxiosDataSource()
     httpDataSource.setName('master')
     httpDataSource.setReadOnly(false)
-    httpDataSource.setUrl('http://localhost:8080')
+    httpDataSource.setUrl('http://api.potens.top')
     httpDataSource.build()
     return httpDataSource
   }
